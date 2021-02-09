cc8799c9ba6ad14d73f3888834563a5f0b1ed85a
ChabaJosa <chabagjg@gmail.com>
2021-02-09 14:02:01 -0500

    First Commit

---
+++ constants/Images.js
@@ -0,0 +1,33 @@
+const Onboarding = 'https://images.unsplash.com/photo-1505995433366-e12047f3f144?fit=crop&w=840&q=80';
+const Pro = 'https://images.unsplash.com/photo-1485796826113-174aa68fd81b?fit=crop&w=840&q=80';
+const Products = {
+  'Accessories': 'https://source.unsplash.com//l1MCA0VyNrk/840x840',
+};
+
+// const Profile = 'https://images.unsplash.com/photo-1512529920731-e8abaea917a5?fit=crop&w=840&q=80';
+const Profile = 'https://pbs.twimg.com/media/EE1oPLqXsAE9gI7.jpg';
+const Avatar = 'https://images.unsplash.com/photo-1518725522904-4b3939358342?fit=crop&w=210&q=80';
+
+const Viewed = [
+  'https://blog.satair.com/hubfs/Satair2018/Content%20dec%202018/ten%20risk%20in%20avaiation%201.jpg',
+  'https://lh3.googleusercontent.com/proxy/qNSYLo8Vt-j6BqnDbgQDW_P6tSal1G30dpbGFjpqZe1TOSF5jRbpx9_I4LDMPVmyXFbs6u91naI1T7FLEqYlY6CxdBDbiugeIQBm5pdrE9fslb75iUPMkuS7BXyFR1TMcbproKtbaTx7-GawviEg',
+  'https://embed-fastly.wistia.com/deliveries/0015b598def9638dc447f321bffcbd95c4df37ca.webp?image_crop_resized=1920x1080',
+  'https://cla.aero/wp-content/uploads/2020/08/corporate_e5nfwa_c_scalew_1440-1.jpg',
+  'https://www.aopa.org/-/media/Images/Legacy/AOPA/Home/News/All/2012/February/Fueling-traps/1202p-fuel.jpg?w=480&h=320&as=1&hash=9EE67B1B3F0162E3724041F2ADD67D74',
+  'https://www.leighnoraircraft.com/wp-content/uploads/2020/06/Fuel01.jpg'
+  // 'https://images.unsplash.com/photo-1508264443919-15a31e1d9c1a?fit=crop&w=240&q=80',
+  // 'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?fit=crop&w=240&q=80',
+  // 'https://images.unsplash.com/photo-1487376480913-24046456a727?fit=crop&w=240&q=80',
+  // 'https://images.unsplash.com/photo-1494894194458-0174142560c0?fit=crop&w=240&q=80',
+  // 'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?fit=crop&w=240&q=80',
+  // 'https://images.unsplash.com/photo-1542068829-1115f7259450?fit=crop&w=240&q=80',
+];
+
+export default {
+  Onboarding,
+  Pro,
+  Products,
+  Profile,
+  Viewed,
+  Avatar,
+}
